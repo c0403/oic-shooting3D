68d24f4cff64feb70429e82264b1372c97753cf6
mukaiutishouta <c0403@oic.jp>
2022-05-20 10:40:47 +0900

    9-3まで完了

--- Project/Player.h
+++ Project/Player.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include "GameDefine.h"
+#include "PlayerShot.h"
 
 // ˆÚ“®‘¬“x
 #define PLAYER_SPEED		(0.1f)
+
+#define PLAYERSHOT_COUNT	(40)
+
+#define PLAYERSHOT_WAIT		(5)
 
 class CPlayer{
 private:
@@ -11,6 +16,9 @@ private:
 	CVector3		m_Pos;
 	float			m_RotZ;
 	float			m_Speed;
+	CMeshContainer	m_ShotMesh;
+	CPlayerShot		m_ShotArray[PLAYERSHOT_COUNT];
+	int				m_ShotWait;
 public:
 	CPlayer();
 	~CPlayer();
